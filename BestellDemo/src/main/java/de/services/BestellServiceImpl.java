2ea1db71997fe3cf0fa82e4647fa3ef15b78bb45
limagoHub <limagoHub@limago.de>
2020-07-21 14:17:59 +0200

    plus beschreibung

--- BestellDemo/src/main/java/de/services/BestellServiceImpl.java
+++ BestellDemo/src/main/java/de/services/BestellServiceImpl.java
@@ -18,6 +18,18 @@ public class BestellServiceImpl implements BestellService {
 
 
 
+	/*
+	 * 1. Bestellung darf nicht null sein
+	 * 2. Creditcard darf nicht null sein
+	 * 3. saldo muss größer gleich null sein
+	 * 4. creditcard muss genau 11 Zeichen
+	 * 5. creditcard darf nur mit M oder V beginnen
+	 * 6. creditcard muss mit genau 10 Ziffern enden.
+	 * 7. Technische Fehler Fehler werden in  BestellServiceException gewandelt
+	 * 8. Bonitätsprüfung -> Erfolg = Bestellung speichern, KundeIstPleiteException sonst
+	 * 
+	 * (zusatzaufgabe. Verify in Order)(zusatzaufgabe. Mockmethoden sollen je genau einmal aufgerufen)
+	 */
 	@Override
 	public void bestellen(Bestellung bestellung, String creditcard, double saldo) throws BestellServiceException, KundeIstPleiteException {
 		
