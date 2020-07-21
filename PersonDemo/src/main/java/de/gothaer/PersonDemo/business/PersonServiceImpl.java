07ee15b9d25a5a6a31fe2ac0a0a9cf1e0d3d3933
limagoHub <limagoHub@limago.de>
2020-07-21 11:56:32 +0200

    antipathen

--- PersonDemo/src/main/java/de/gothaer/PersonDemo/business/PersonServiceImpl.java
+++ PersonDemo/src/main/java/de/gothaer/PersonDemo/business/PersonServiceImpl.java
@@ -1,16 +1,19 @@
 package de.gothaer.PersonDemo.business;
 
+import java.util.List;
+
 import de.gothaer.PersonDemo.Persistence.PersonRepository;
 import de.gothaer.PersonDemo.Persistence.entities.Person;
 
 public class PersonServiceImpl implements PersonService {
 
 	private final PersonRepository personRepository;
-	
+	private final List<String> antipathen;
 	
 
-	public PersonServiceImpl(final PersonRepository personRepository) {
+	public PersonServiceImpl(final PersonRepository personRepository, final List<String> antipathen) {
 		this.personRepository = personRepository;
+		this.antipathen = antipathen;
 	}
 
 
@@ -21,14 +24,54 @@ public class PersonServiceImpl implements PersonService {
 	 * 3. Vorname muss min 2 Zeichen enthalten
 	 * 4. Nachname darf nicht null sein
 	 * 5. Nachname muss min 2 Zeichen haben
-	 * 6. Vorname darf nicht Attila sein
+	 * 6. Vorname darf kein Antipath sein...
 	 * 7. Person darf erst gespeichert werden wenn Bedingungen erfüllt sind
 	 * 8. Egal welcher technischer Fehler auftritt, es soll immer in eine PersonenServiceException gewandelt werden. 
 	 */
 	@Override
 	public void speichern(Person person) throws PersonServiceException {
 		
+		try {
+			speichernImpl(person);
+		} catch (RuntimeException e) {
+			// ins logfile schreiben
+			throw new PersonServiceException("Person konnte nicht gepeichert werden.");
+		}
+	}
 
+
+
+	private void speichernImpl(Person person) throws PersonServiceException {
+		checkPerson(person);
+		personRepository.save(person);
+	}
+
+
+
+	private void checkPerson(Person person) throws PersonServiceException {
+		plausibilitaetsPruefung(person);
+		
+		vaidierung(person);
+	}
+
+
+
+	private void vaidierung(Person person) throws PersonServiceException {
+		if(antipathen.contains(person.getVorname()))
+			throw new PersonServiceException("Antipath.");
+	}
+
+
+
+	private void plausibilitaetsPruefung(Person person) throws PersonServiceException {
+		if(person == null)
+			throw new PersonServiceException("person must not be null");
+		
+		if(person.getVorname() == null || person.getVorname().length() < 2)
+			throw new PersonServiceException("firstname too short.");
+		
+		if(person.getNachname() == null || person.getNachname().length() < 2)
+			throw new PersonServiceException("lastname too short.");
 	}
 
 }
--- PersonDemo/src/test/java/de/gothaer/PersonDemo/business/PersonServiceImplTest.java
+++ PersonDemo/src/test/java/de/gothaer/PersonDemo/business/PersonServiceImplTest.java
@@ -1,15 +1,133 @@
 package de.gothaer.PersonDemo.business;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import de.gothaer.PersonDemo.Persistence.PersonRepository;
+import de.gothaer.PersonDemo.Persistence.entities.Person;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PersonServiceImplTest {
 	
+	@InjectMocks private PersonServiceImpl objectUnderTest;
+	@Mock private PersonRepository personRepositoryMock;
+	@Mock private List<String> antipathenMock;
+	
+//	@Before
+//	public void setUp() {
+//		personRepositoryMock = Mockito.mock(PersonRepository.class);
+//		objectUnderTest = new PersonServiceImpl(personRepositoryMock);
+//	}
+	
 	@Test
-	public void test() {
-		System.out.println("It works!");
+	public void speichern_inValidParameterNull_throwsPersonenServiceException()  {
+		try {
+			objectUnderTest.speichern(null);
+			fail("Exception expected!");
+		} catch (PersonServiceException e) {
+			assertEquals("person must not be null", e.getMessage());
+		}
+	}
+
+	@Test
+	public void speichern_WrongFirstnameNull_throwsPersonenServiceException()  {
+		try {
+			final Person person = new Person(null, "Doe");
+			
+			objectUnderTest.speichern(person);
+			fail("Exception expected!");
+		} catch (PersonServiceException e) {
+			assertEquals("firstname too short.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void speichern_WrongFirstnameTooShort_throwsPersonenServiceException()  {
+		try {
+			final Person person = new Person("J", "Doe");
+			
+			objectUnderTest.speichern(person);
+			fail("Exception expected!");
+		} catch (PersonServiceException e) {
+			assertEquals("firstname too short.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void speichern_WrongLastnameNull_throwsPersonenServiceException()  {
+		try {
+			final Person person = new Person("John", null);
+			
+			objectUnderTest.speichern(person);
+			fail("Exception expected!");
+		} catch (PersonServiceException e) {
+			assertEquals("lastname too short.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void speichern_WrongLastnameTooShort_throwsPersonenServiceException()  {
+		try {
+			final Person person = new Person("John", "D");
+			
+			objectUnderTest.speichern(person);
+			fail("Exception expected!");
+		} catch (PersonServiceException e) {
+			assertEquals("lastname too short.", e.getMessage());
+		}
+	}
+
+
+
+	@Test
+	public void speichern_Antipath_throwsPersonenServiceException()  {
+		try {
+			final Person person = new Person("Fritz", "Doe");
+			
+			when(antipathenMock.contains(anyString())).thenReturn(true);
+			
+			objectUnderTest.speichern(person);
+			fail("Exception expected!"); 
+		} catch (PersonServiceException e) {
+			assertEquals("Antipath.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void speichern_ExceptionInUnderlyingServe_throwsPersonenServiceException()  {
+		try {
+			// Arrange
+			final Person person = new Person("John", "Doe");
+			
+			doThrow(new ArrayIndexOutOfBoundsException()).when(personRepositoryMock).save((Person) any());
+			when(antipathenMock.contains(anyString())).thenReturn(false);
+			// Action
+			objectUnderTest.speichern(person);
+			fail("Exception expected!");
+		} catch (PersonServiceException e) {
+			assertEquals("Person konnte nicht gepeichert werden.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void speichern_HappyDay_PersonSavedInPersistenceLayer() throws Exception{
+		final Person person = new Person("John", "Doe");
+		when(antipathenMock.contains(anyString())).thenReturn(false);
+		objectUnderTest.speichern(person);
+		verify(personRepositoryMock).save(person);
 	}
 
 }
+ 
